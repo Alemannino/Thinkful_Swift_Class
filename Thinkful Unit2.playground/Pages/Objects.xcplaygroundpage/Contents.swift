bdcb1f30a8110961c788e71aac1b8579647a48aa
Alessandro Mannino <alemannino19@gmail.com>
2016-11-22 21:43:59 -0500

    Added init variations

--- Thinkful Unit2.playground/Pages/Objects.xcplaygroundpage/Contents.swift
+++ Thinkful Unit2.playground/Pages/Objects.xcplaygroundpage/Contents.swift
@@ -18,20 +18,43 @@ class Person {
     var gender:String
     var profession:String
     var location:String
-    init(name:String, gender:String, profession:String, location:String) {
+    var age:Int
+    init(name:String, gender:String, profession:String, location:String, age:Int) {
         self.name = name
         self.gender = gender
         self.profession = profession
         self.location = location
+        self.age = age
     }
+    init(name: String) {
+        self.name = name
+        self.gender = "Unknown"
+        self.profession = "Unknown"
+        self.location = "Unknown"
+        self.age = 0
+    }
+    init(profession: String) {
+        self.profession = profession
+        self.name = "Unknown"
+        self.gender = "Unknown"
+        self.location = "Unknown"
+        self.age = 0
+        
+    }
+    
 }
-
-let peter = Person(name: "Peter", gender: "Male", profession: "Computer Developer", location: "New York")
-
+    
 
 
+let peter = Person(name: "Peter", gender: "Male", profession: "Computer Developer", location: "New York", age: 25)
 
+let alessandro = Person(name: "Alessandro", gender: "Male", profession: "Product Manager", location: "New Jersey", age: 24)
 
+let chelsey = Person(name: "Chelsey", gender: "Female", profession: "Social Media Management", location: "Female", age: 24)
+
+let judy = Person(name: "Judy")
+
+let vera = Person(profession: "Pizzeria")
 
 
 
